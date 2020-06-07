fba451d8f2f7e964b958342c3edfe29fcd677b24
shirleyiscool <xueyingli66@gmail.com>
2020-06-07 11:00:31 -0700

    Updates

---
+++ precache-manifest.9a904820776fe02b1dfda1a206505e94.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "28cdb9efdc1a62bcc10b4a90baa4a32a",
+    "url": "/React_Animal_Crossing/index.html"
+  },
+  {
+    "revision": "f142eca28224f2c2e0e8",
+    "url": "/React_Animal_Crossing/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "fb26dbd61d9cec122665",
+    "url": "/React_Animal_Crossing/static/css/main.a704787f.chunk.css"
+  },
+  {
+    "revision": "f142eca28224f2c2e0e8",
+    "url": "/React_Animal_Crossing/static/js/2.69cd8f67.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_Animal_Crossing/static/js/2.69cd8f67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb26dbd61d9cec122665",
+    "url": "/React_Animal_Crossing/static/js/main.474fd61c.chunk.js"
+  },
+  {
+    "revision": "5c3fa6c469944e0a2dfc",
+    "url": "/React_Animal_Crossing/static/js/runtime-main.36b3a764.js"
+  },
+  {
+    "revision": "372cc1d42335e38f4cf56df6091a73cb",
+    "url": "/React_Animal_Crossing/static/media/CHERI.372cc1d4.woff"
+  }
+]);
